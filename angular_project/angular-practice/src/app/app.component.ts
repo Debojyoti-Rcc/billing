2306d3afdc4cae292b28f9825c0496a0628b7ba4
DESKTOP-ELII2RM\deboj <debojyoti1112@gmail.com>
2018-09-07 22:40:50 +0530

    Angular Project pushed to repository.

---
+++ angular_project/angular-practice/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-practice';
+  //var myObject = "Hello I am kesto!!";
+}
